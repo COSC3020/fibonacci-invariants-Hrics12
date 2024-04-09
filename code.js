9614428dec36f690494d37a78e997cfef1dbb16d
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-04-09 09:57:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,18 +1,17 @@
 function fib(n) {
-    cost array = []
+    const array = []
     let Num1 = 1
     let Num2 = -1
 
     let ans;
-    for(i=0;i<=n;i++){
-        ans= Num2 + Num1;
+    for (i = 0; i <= n; i++) {
+        ans = Num2 + Num1;
         Num2 = Num1;
-        Num1=ans;
+        Num1 = ans;
         array.push(Num1)
     }
     array.shift()
     return array;
-
-    
-    return n;
 }
+
+console.log(fib(7))
