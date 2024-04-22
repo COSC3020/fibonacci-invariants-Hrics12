eacebe32b7c254740ff14b292001d1fffc90a9d9
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-04-22 09:46:21 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function fib(n) {
-    return n;
+    if (n <= 2) return Array(n).fill(1);
+    let fibArray = [1, 1];
+    function helper(i) {
+        if (i === n) return;
+        fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        helper(i + 1);
+    }
+    helper(2);
+    return fibArray;
 }
+let result = fib(10);
+console.log(result);
